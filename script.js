711e5c2ca461acd6fbbd19e6f5c26a80c5ccaf43
jasonlam514 <jasonlam514@hotmail.com>
2018-02-06 04:20:41 +0000

    scrollify added

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
